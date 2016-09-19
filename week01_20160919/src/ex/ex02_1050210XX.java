8781d6378f3fc34938cc8f5d62acd8661c9e13df
Yi-Cheng Chen <Chad.CYB@gmail.com>
2016-09-19 13:46:10 +0800

    week01_init

---
+++ week01_20160919/src/ex/ex02_1050210XX.java
@@ -0,0 +1,15 @@
+package ex;
+/*
+ * Topic: 請寫一個程式讓使用者輸入體重單位 "公斤"，轉換成 "磅" (1磅 = 0.45359 公斤)
+ * Date: 2016/09/19
+ * Author: 1050210XX 周永振老師
+ */
+
+public class ex02_1050210XX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
